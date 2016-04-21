0f2519df9d3259213e2b8499fe4e4dbbb6e4b607
AnujTewari <anuj.tewari88@gmail.com>
2016-04-20 19:06:13 -0500

    adding changes for sorting docs in a year

--- core/src/com/irwebapp/pkg/ProcessSearchResult.java
+++ core/src/com/irwebapp/pkg/ProcessSearchResult.java
@@ -4,12 +4,22 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
-import org.json.*;
-import java.util.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.resource.Document;
 
 public class ProcessSearchResult {
-	String search;
+
+	private String search;
 	private final String USER_AGENT = "Mozilla/5.0 (Windows NT 5.1; rv:19.0) Gecko/20100101 Firefox/19.0";
 
 	public ProcessSearchResult(String search) {
@@ -49,34 +59,73 @@ public class ProcessSearchResult {
 		return content;
 
 	}
-	
-	public  static HashMap<String, Object> getContenForTimeTimeLine(String content){
+
+	public static HashMap<String, Object> getContenForTimeTimeLine(
+			String content) {
 		HashMap<String, Object> result = new HashMap<String, Object>();
-		try{
-			JSONObject jo = new JSONObject(content);
-			result.put("time", jo.getJSONObject("responseHeader").get("QTime").toString());
-			JSONArray arr = jo.getJSONObject("response").getJSONArray("docs");
-			Map<String, List<String>> year_docidMap = new TreeMap<String, List<String>>(Collections.reverseOrder());
-			for (int i = 0; i < arr.length(); i++) {
-				String year = arr.getJSONObject(i).getJSONArray("date").getString(0).substring(0, 4);
-				String docid = arr.getJSONObject(i).getJSONArray("lead_paragraph").getString(0);
-				// System.out.println("Year :" + year + " Doc ID :" + docid);
-				List<String> list = null;
-				if (year_docidMap.containsKey(year)) {
-					list = year_docidMap.get(year);
+		try {
+
+			JSONObject jsonObject = new JSONObject(content);
+			result.put("respones_time_in_millisecond", jsonObject
+					.getJSONObject("responseHeader").get("QTime").toString());
+			JSONArray arrayOfDocsAsJSON = jsonObject.getJSONObject("response")
+					.getJSONArray("docs");
+			Map<String, List<Document>> year_docMap = new TreeMap<String, List<Document>>(
+					Collections.reverseOrder());
+			for (int i = 0; i < arrayOfDocsAsJSON.length(); i++) {
+
+				JSONObject JSONObjectForDoc = arrayOfDocsAsJSON
+						.getJSONObject(i);
+
+				String year = JSONObjectForDoc.getJSONArray("date")
+						.getString(0).substring(0, 4);
+
+				double ratingForDoc = getRatingForDoc((String) JSONObjectForDoc
+						.get("id"));
+
+				Document newDoc = new Document(JSONObjectForDoc, ratingForDoc);
+
+				List<Document> doc_list = null;
+				if (year_docMap.containsKey(year)) {
+					doc_list = year_docMap.get(year);
 				} else {
-					list = new ArrayList<String>();
+					doc_list = new ArrayList<Document>();
 				}
-				list.add(docid);
-				year_docidMap.put(year, list);
+				doc_list.add(newDoc);
+				year_docMap.put(year, doc_list);
 			}
-			
-			result.put("year_docidMap", year_docidMap);
-			result.put("numOfResponse", jo.getJSONObject("response").get("numFound").toString());
-		}catch(Exception ex){
+
+			for (String year : year_docMap.keySet()) {
+				List<Document> eachDocList = year_docMap.get(year);
+				Collections.sort(eachDocList, new MyComparator());
+			}
+			result.put("year_docMap", year_docMap);
+			result.put("numOfResponse", jsonObject.getJSONObject("response")
+					.get("numFound").toString());
+		} catch (Exception ex) {
+			System.out
+					.println("Error while getting the content from the server.");
 			ex.printStackTrace();
 		}
 		return result;
+	}
+
+	public static double getRatingForDoc(String docID) {
+		return 1.0;
+	}
+
+	static class MyComparator implements Comparator<Document> {
+
+		@Override
+		public int compare(Document doc1, Document doc2) {
+			if (doc2.getDocRating() > doc1.getDocRating()) {
+				return 1;
+			} else if (doc2.getDocRating() < doc1.getDocRating()) {
+				return -1;
+			} else {
+				return 0;
+			}
+		}
 	}
 
 }
